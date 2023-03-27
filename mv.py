4591f478da3281aa9d5d9fd7701a95cb90a5d13c
عبدالله الحديدي <abdallah201@protonmail.com>
2023-03-27 22:34:05 +0200

    deleted mv.py

--- mv.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-
-for root, dirs, files in os.walk("./PL1DUmT", topdown=False):
-    for name in files:
-        if name.__contains__("Java Programming for Beginners"):
-            shutil.move('PL1DUmT/'+name, 'Intro/'+name)
-        else:
-            shutil.move('PL1DUmT/'+name, 'OOP/'+name)
